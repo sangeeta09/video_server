c51aa85088d1b2493d7cf2c89b877c26a25c1f2c
anuj_personal <anuj_personal@AnujMaurya-Air.local>
2020-06-22 10:05:44 +0530

    removed unused func

--- internal/delivery/routes.go
+++ internal/delivery/routes.go
@@ -13,9 +13,7 @@ import (
 //InitHandlers inits all the routes
 func InitHandlers() *mux.Router {
 
-	// r := mux.NewRouter().StrictSlash(true)
 	r := mux.NewRouter()
-	// r.Use(CommonMiddleware)
 
 	//healthcheck api
 	r.HandleFunc("/healthcheck", controllers.HealthCheck).Methods("GET")
@@ -29,15 +27,4 @@ func InitHandlers() *mux.Router {
 	r.Handle("/user/{id}", auth.JwtVerify(http.HandlerFunc(controllers.DeleteUser))).Methods("DELETE")
 
 	return r
-}
-
-// CommonMiddleware --Set content-type
-func CommonMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
-		next.ServeHTTP(w, r)
-	})
 }
